42ea5eeb46e738ef7100470d717a5545ad511585
taol-assignment <taol@mun.ca>
2019-11-18 03:56:48 -0330

    Reflective value validation.

--- lib/MetricCalculation.R
+++ lib/MetricCalculation.R
@@ -1,9 +1,40 @@
 library(pavo)
 library(rjson)
 
-filePath <- commandArgs(trailingOnly=TRUE)[1]
+filePath <- commandArgs(trailingOnly = TRUE)[1]
 
 spectra <- getspec(where = filePath, ext = 'Master.Transmission')
+
+files = colnames(spectra)
+files = files[files != "wl"]
+
+errors <- vector(mode = "list")
+warnings <- vector(mode = "list")
+
+for (i in 1 : nrow(spectra)) {
+    row <- spectra[i,]
+
+    for (file in files) {
+        full_name <- paste(file, '.Master.Transmission', sep = '')
+        val = row[[file]]
+
+        if (val <= 0 && val >= - 2) {
+            warnings <- c(errors, list(
+            file = full_name,
+            wavelen = row$wl,
+            value = val
+            ))
+        } else if (val < - 2) {
+            errors <- c(errors, list(
+            file = full_name,
+            wavelen = row$wl,
+            value = val
+            ))
+        }
+    }
+}
+
+write(toJSON(list(warnings=warnings, errors=errors)), stderr())
 
 objective_metrics <- summary(spectra)
 
