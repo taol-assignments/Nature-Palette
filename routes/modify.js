f6617d2f920ae496bb7aff86ffae5085d0729990
Shuaishuai Li <shuaishuail@mun.ca>
2019-11-20 23:48:38 -0330

    upload modified raw files and metadata

--- routes/modify.js
+++ routes/modify.js
@@ -1,5 +1,8 @@
-let express = require('express');
-let Submission = require('../models/Submission');
+const express = require('express');
+const util = require('util');
+const Submission = require('../models/Submission');
+const multer = require('multer');
+const mkdirp = util.promisify(require('mkdirp'));
 
 let router = express.Router();
 
@@ -16,6 +19,33 @@ router.get('/:id', async function (req, res) {
     res.render('modify/modifyFile', {
         submissionId: submissionId
     });
+})
+
+router.post('/upload', function (req, res, next) {
+
+    next();
+}, multer({
+    storage: multer.diskStorage({
+        destination: async function (req, file, cb) {
+            let submissionId = req.body['submissionId'];
+            let dir = __dirname + "/../uploads/Temp/" + submissionId;
+            try {
+                await mkdirp(dir);
+                cb(null, dir);
+            } catch (e) {
+                cb(e);
+            }
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.originalname)
+        }
+    })
+}).fields([{
+    name: 'metadata', maxCount: 1
+}, {
+    name: 'rawFile', maxCount: 1
+}]), async function (req, res) {
+    res.render('modify/successPanel');
 })
 
 module.exports = router;
